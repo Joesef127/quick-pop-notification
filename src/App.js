4b4c95e35a412439b9446c6b4b43e833fa57b898
Adedayo Adegbola <adegboladayor@gmail.com>
2024-06-05 03:13:55 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [notificationMessage, setNotificationMessage] = useState('');
 
   const triggerNotification = () => {
-    setNotificationMessage('this is a sample notifiation message');
+    setNotificationMessage('This is a sample notification message');
   };
 
   const handleClose = () => {
@@ -13,13 +13,18 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
       <button onClick={triggerNotification}>Trigger Notification</button>
       {notificationMessage && (
         <NotificationPanel
           message={notificationMessage}
           onClose={handleClose}
-          animationDuration={5000}
+          animationDuration={3000}
         />
       )}
     </div>
